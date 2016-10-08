773c1216b7c909a15317ac5c385773f1a30183fc
Wilker <Wilker.rj@gmail.com>
2016-10-08 17:06:09 -0300

    Git Ignore
    Removido arquivos netbeans

---
+++ T2-RSA/rsa/rsa.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   rsa.c
+ * Author: wilker
+ *
+ * Created on 8 de Outubro de 2016, 16:55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ T2-RSA/rsa/rsa.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   rsa.h
+ * Author: wilker
+ *
+ * Created on 8 de Outubro de 2016, 16:56
+ */
+
+#ifndef RSA_H
+#define RSA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RSA_H */
+
