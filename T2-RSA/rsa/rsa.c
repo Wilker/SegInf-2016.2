34d3abb46824cf3c6119e43554100a3bbec12577
Wilker <Wilker.rj@gmail.com>
2016-10-17 22:00:20 -0200

    Esqueleto iniciado

--- T2-RSA/rsa/rsa.c
+++ T2-RSA/rsa/rsa.c
@@ -17,8 +17,41 @@
 /*
  * 
  */
+int findPrimes(int n, int *p1, int *p2);
+
+void decrypt(int p1,int p2,int c);
 int main(int argc, char** argv) {
+    //Chave pública
+    int n = 0;
+    //expoente de encriptação
+    int e = 0;
+    //mensagem cifrada
+    int c = 0;
+
+    scanf("%d %d %d", &n, &e, &c);
+    //primo 1
+    int p1 = 0;
+    //primo 2
+    int p2 = 0;
+    if (findPrimes(n, &p1, &p2)) {
+
+        decrypt(p1, p2, c) {
+
+
+        }
+    }
+
 
     return (EXIT_SUCCESS);
 }
+
+/**
+ * Retorna 1 caso encontre p1*p1=n  e 0 caso contrário
+ * @param n
+ * @return 
+ */
+int findPrimes(int n, int *p1, int *p2) {
+    return 1;
+}
+
 
