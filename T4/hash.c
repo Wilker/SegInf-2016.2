16a3c58ccc1a23ac53d4bebd38b2a23d549a821b
Wilker <Wilker.rj@gmail.com>
2016-12-20 01:32:20 -0200

    Função de leitura da linha adicionada

--- T4/hash.c
+++ T4/hash.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stlib.h>
+#include <math.h>
+
+void printVetor(int* vetor, int n);
 
 int main(int argc, char** argv) {
+/* */
+    /* Leitura do número de bits (k) de paridade e do número de valores de hash a serem calculados (d) */
+    int k, d;
+    scanf("%d %d", &k, &d);
 
 
-return 0;
+    /* Cálculo de n, que determinará a quantidade de bits das linhas*/
+    int n = pow(2,k) - 1 - k;
+
+
+
+    return 0;
+}
+
+/* Lê a k vezes a quantidade de n bits e retona um vetor com os bits lidos */
+int* read(int k, int n) {
+    int i;
+
+    int* bits = (int*) malloc(n * sizeof(int));
+    for (i = 0; i < k; i++) {
+        scanf("%d", bits[i]);
+    }
+}
+
+
+void printVetor(int* vetor, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf(" %d", vetor[i]);
+    }
+    printf("\n");
 }
